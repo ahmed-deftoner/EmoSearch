cdd1f4a052aa7f2e829eb9eeeb9e8bd22a674f35
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-11-25 14:08:46 +0500

    features

--- pkg/audioFeature/audiofeatures.go
+++ pkg/audioFeature/audiofeatures.go
@@ -8,16 +8,25 @@ import (
 	"github.com/zmb3/spotify/v2"
 )
 
-type MyArr struct {
-	Song  string
-	value float32
+type Features struct {
+	Song         string
+	valence      float32
+	danceable    float32
+	intense      float32
+	instrumental float32
+	vocal        float32
+	acoustic     float32
+}
+
+func GetSongs() {
+
 }
 
 func GetSadSongs(ctx context.Context, results *spotify.SearchResult, client *spotify.Client) []string {
 	if results.Albums != nil {
 		item := results.Albums.Albums[0]
 		res, err := client.GetAlbumTracks(ctx, item.ID, spotify.Market("US"))
-		arr := make([]MyArr, res.Total+1)
+		arr := make([]Features, res.Total+1)
 		var i int = 0
 
 		if err != nil {
@@ -29,13 +38,13 @@ func GetSadSongs(ctx context.Context, results *spotify.SearchResult, client *spo
 				fmt.Println("error getting audio features...", err.Error())
 			}
 			arr[i].Song = item.Name
-			arr[i].value = x[0].Valence
+			arr[i].valence = x[0].Valence
 			//fmt.Println(arr[i])
 			i++
 		}
 
 		sort.Slice(arr, func(i, j int) bool {
-			return arr[i].value < arr[j].value
+			return arr[i].valence < arr[j].valence
 		})
 		songArr := make([]string, res.Total+1)
 
