fa1387b8ecf131a3c885c60cbeb37b1289898c75
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-11-25 14:14:46 +0500

    get intense

--- pkg/audioFeature/audiofeatures.go
+++ pkg/audioFeature/audiofeatures.go
@@ -30,6 +30,12 @@ func GetHappySongs(arr []Features) {
 	})
 }
 
+func GetIntenseSongs(arr []Features) {
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i].intense > arr[j].intense
+	})
+}
+
 func GetSongs(ctx context.Context, results *spotify.SearchResult, client *spotify.Client) []string {
 	if results.Albums != nil {
 		item := results.Albums.Albums[0]
